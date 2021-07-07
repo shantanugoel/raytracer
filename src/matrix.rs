26aac7afc5672e6ce4887948674d8138502a90f4
Shantanu Goel <shantanu.goel@gmail.com>
2021-07-07 21:08:51 +0530

    Fix tests

--- src/matrix.rs
+++ src/matrix.rs
@@ -40,25 +40,26 @@ mod tests {
 
     #[test]
     fn test_matrix_creation_default() {
-        let mut m2x2 = Matrix::<f64>::new(2, 2);
-        m2x2[0][0] = 1.0;
-        m2x2[0][1] = 2.0;
-        m2x2[1][0] = 3.0;
-        m2x2[1][1] = 4.0;
-        assert_eq!(4, m2x2.data.capacity());
+        let num_rows = 2;
+        let num_cols = 2;
+        let mut m2x2 = Matrix::<f64>::new(num_rows, num_cols);
+        let expected_vec = vec![0.0, 0.0, 0.0, 0.0];
+        assert_eq!(num_rows * num_cols, m2x2.data.capacity());
+        assert_eq!(num_rows * num_cols, m2x2.data.len());
+        assert_eq!(expected_vec, m2x2.data);
     }
 
     #[test]
     fn test_matrix_assignment() {
         let num_rows = 2;
         let num_cols = 3;
-        let mut m2x2 = Matrix::<f64>::new(num_rows, num_cols);
+        let mut m = Matrix::<f64>::new(num_rows, num_cols);
         let expected_vector = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         for row in 0..num_rows {
             for col in 0..num_cols {
-                m2x2[row][col] = expected_vector[row * num_cols + col];
+                m[row][col] = expected_vector[row * num_cols + col];
             }
         }
-        assert_eq!(expected_vector, m2x2.data);
+        assert_eq!(expected_vector, m.data);
     }
 }
