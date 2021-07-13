32edf3140858cc9cd89dbc82b06b0871a0719c9d
Shantanu Goel <shantanu.goel@gmail.com>
2021-07-13 20:57:18 +0530

    Go back to full precision float comparisons

--- src/lib/utils.rs
+++ src/lib/utils.rs
@@ -1,13 +1,11 @@
-use num::{Float, NumCast};
+use num::Float;
 
 /// Compare two floats for equality
 pub fn is_eq_float<T>(x: &T, y: &T) -> bool
 where
     T: Float,
 {
-    // Use a custom epsilon instead of Float::epsilon() to limit precision checks
-    let epsilon: T = NumCast::from(0.000_01).unwrap();
-    if (*x - *y).abs() <= epsilon {
+    if (*x - *y).abs() <= Float::epsilon() {
         return true;
     }
     false
