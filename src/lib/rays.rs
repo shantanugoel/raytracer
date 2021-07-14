2846902bcd67d9d54073fbca25b67ecbefd18ebc
Shantanu Goel <shantanu.goel@gmail.com>
2021-07-14 17:47:10 +0530

    Ray transformation

--- src/lib/rays.rs
+++ src/lib/rays.rs
@@ -1,4 +1,7 @@
-use crate::tuple::{Point, Vector};
+use crate::{
+    matrix::{Matrix, MatrixError},
+    tuple::{Point, Vector},
+};
 
 pub struct Ray {
     pub origin: Point,
@@ -12,6 +15,13 @@ impl Ray {
 
     pub fn position(&self, t: f64) -> Point {
         self.origin + self.direction * t
+    }
+
+    pub fn transform(&self, m: Matrix<f64>) -> Result<Self, MatrixError> {
+        Ok(Ray {
+            origin: (m.clone() * self.origin)?,
+            direction: (m.clone() * self.direction)?,
+        })
     }
 }
 
@@ -29,5 +39,23 @@ mod tests {
         assert_eq!(Point::new(3., 3., 4.), ray.position(1.));
         assert_eq!(Point::new(1., 3., 4.), ray.position(-1.));
         assert_eq!(Point::new(4.5, 3., 4.), ray.position(2.5));
+    }
+
+    #[test]
+    fn test_translate_ray() {
+        let ray = Ray::new(Point::new(1.0, 2.0, 3.0), Vector::new(0.0, 1.0, 0.0));
+        let m = Matrix::translation(3.0, 4.0, 5.0);
+        let ray2 = ray.transform(m).unwrap();
+        assert_eq!(Point::new(4.0, 6.0, 8.0), ray2.origin);
+        assert_eq!(Vector::new(0.0, 1.0, 0.0), ray2.direction);
+    }
+
+    #[test]
+    fn test_scale_ray() {
+        let ray = Ray::new(Point::new(1.0, 2.0, 3.0), Vector::new(0.0, 1.0, 0.0));
+        let m = Matrix::scaling(2.0, 3.0, 4.0);
+        let ray2 = ray.transform(m).unwrap();
+        assert_eq!(Point::new(2.0, 6.0, 12.0), ray2.origin);
+        assert_eq!(Vector::new(0.0, 3.0, 0.0), ray2.direction);
     }
 }
